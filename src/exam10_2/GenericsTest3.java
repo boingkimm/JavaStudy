b92b7603bda94e3ba7c0cbebe95c7cd831150e46
Kim Bohee <boingkimm@gmail.com>
2023-07-25 12:05:54 +0900

    Generics - <?>

---
+++ src/exam10_2/GenericsTest3.java
@@ -0,0 +1,30 @@
+package exam10_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// <?>
+public class GenericsTest3 {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("홍길동");
+		list.add("이순신");
+		
+		printData(list);
+		
+		List<Integer> list2 = new ArrayList<Integer>();
+		list2.add(10);
+		list2.add(20);
+		
+		printData(list2);
+		
+	}
+	
+	// <?> : 컬렉션 타입의 generic이면 다 받아줌 (List의 타입)
+	public static void printData(List<?> xxx) {
+		System.out.println(xxx);
+	}
+	
+}
